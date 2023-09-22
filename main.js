c85d7fbb003f0e7de346fcb33699062a96825de6
yoochan0108 <yoochan0108@naver.com>
2023-09-22 15:38:16 +0900

    동적인 요소에 이벤트적용

--- main.js
+++ main.js
@@ -7,6 +7,18 @@ const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxR
 const tit_len = 30;
 const desc_len = 180;
 
+//이벤트 위임(event delegation)
+//동적으로 생성되는 요소에 이벤트 연결이 불가, 이벤트 연결시점에는 해당 돔이 생성되지 않았기 떄문
+//항상 있는 body요소에다가 이벤트를 위임을해서 추루 동적 dom이 생기면 이벤츠를 전달받도록 처리
+
+window.addEventListener('click', (e) => {
+	//e.currentTarget : 이벤트가 연결되어 있는 선택자를 반환
+	//e.target : 실제화면상에서 이벤트가 발생한 요소를 반환
+	if (e.target.nodeName === 'IMG') {
+		console.log('You clicked pic');
+	}
+});
+
 fetch(resultURL)
 	.then((data) => data.json())
 	.then((json) => {
