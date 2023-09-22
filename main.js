90ed5b2d12e7ee456c9515af912ff0ca251365b1
yoochan0108 <yoochan0108@naver.com>
2023-09-22 16:14:43 +0900

    동적인 요소에 이벤트 위임 처리

--- main.js
+++ main.js
@@ -12,11 +12,8 @@ const desc_len = 180;
 //항상 있는 body요소에다가 이벤트를 위임을해서 추루 동적 dom이 생기면 이벤츠를 전달받도록 처리
 
 window.addEventListener('click', (e) => {
-	//e.currentTarget : 이벤트가 연결되어 있는 선택자를 반환
-	//e.target : 실제화면상에서 이벤트가 발생한 요소를 반환
-	if (e.target.nodeName === 'IMG') {
-		console.log('You clicked pic');
-	}
+	if (e.target.nodeName === 'IMG') createPop();
+	if (e.target.className === 'close') removePop();
 });
 
 fetch(resultURL)
@@ -53,3 +50,17 @@ fetch(resultURL)
 
 		frame.innerHTML = tags;
 	});
+
+function createPop() {
+	const aside = document.createElement('aside');
+	aside.innerHTML = `
+   <div class='con'></div>
+  <span class='close'>close</span>
+  `;
+	document.body.append(aside);
+}
+
+function removePop() {
+	const pop = document.querySelector('aside');
+	pop.remove();
+}
