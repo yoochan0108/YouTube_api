75a6fe3615bcd1aa59dca6b598904fa49a855a8b
yoochan0108 <yoochan0108@naver.com>
2023-09-22 14:08:09 +0900

    제목 글자 짜르기

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 const pid = 'PLuPg_rytnr6k-sPrYGREjbXLMZzwE7-N-&si=c72x_0iZr-7tHUMA';
 const num = 5;
 const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+const tit_len = 50;
+
+//일정 글자수 이상일때 글짜자르고 말줄임표 붙이기
+//문자열.substr(시작위치,자를 글자수)
 
 fetch(resultURL)
 	.then((data) => data.json())
@@ -14,7 +18,11 @@ fetch(resultURL)
 		json.items.map((data) => {
 			tags += `
 				<article>
-					<h2>${data.snippet.title}</h2>
+					<h2>${
+						data.snippet.title.length > tit_len
+							? data.snippet.title.substr(0, tit_len) + '...'
+							: data.snippet.title
+					}</h2>
 					<div class='txt'>
 						<p>${data.snippet.description}</p>
 						<span>${data.snippet.publishedAt}</span>
